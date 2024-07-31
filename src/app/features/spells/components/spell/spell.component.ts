dd8e141178f2cf778cda6a01692c67ae0216ee3d
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-07-31 14:36:19 +0200

    Spell component creation

---
+++ src/app/features/spells/components/spell/spell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spell',
+  standalone: true,
+  imports: [],
+  templateUrl: './spell.component.html',
+  styleUrl: './spell.component.scss'
+})
+export class SpellComponent {
+
+}
