4708ed9174fbb5487a1f2f0405aca4a8652dcab5
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-07-31 14:36:24 +0200

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./core/layout/home/home.component";
+import {LoginComponent} from "./features/auth/components/login/login.component";
+import {RegisterComponent} from "./features/auth/components/register/register.component";
+import {SpellComponent} from "./features/spells/components/spell/spell.component";
 
 export const routes: Routes = [
   {
@@ -10,5 +13,17 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'spells',
+    component: SpellComponent
   }
 ];
