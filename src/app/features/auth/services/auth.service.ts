f73d118f76fc7581c90a968505be40071e8ef961
AzzedineHassaini <azzedinehassaini@gmail.com>
2024-08-03 19:41:30 +0200

    logging

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -7,9 +7,6 @@ import {ILoginForm} from "../form/login.form";
 import {IRegisterForm} from "../form/register.form";
 import { environment } from '../../../../environments/environment';
 
-
-const apiUrl = environment.apiUrl;
-
 @Injectable({
   providedIn: 'root'
 })
@@ -52,7 +49,8 @@ export class AuthService {
   }
 
   login(form: ILoginForm): Observable<IAuth> {
-    return this._client.post<IAuth>(`${apiUrl}auth/login`, form).pipe(
+    console.log('API URL:', environment.apiUrl);
+    return this._client.post<IAuth>(`${environment.apiUrl}auth/login`, form).pipe(
       tap((auth) => {
         this.currentUser = auth;
       }),
@@ -79,7 +77,7 @@ export class AuthService {
   }
 
   register(form: IRegisterForm, role: string, login: boolean = true) : Observable<IAuth>{
-    return this._client.post<IAuth>(apiUrl + 'auth/register', form).pipe(
+    return this._client.post<IAuth>(environment.apiUrl + 'auth/register', form).pipe(
       tap((auth) => {
         if (login) {
           this.currentUser = auth
